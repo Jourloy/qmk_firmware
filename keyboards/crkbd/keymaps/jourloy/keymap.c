8e1244748331b4371d887e9a845ec94777dae045
Jourloy <jourloy@yandex.ru>
2022-08-07 12:47:18 +0300

    ✅ Change keymap

--- keyboards/crkbd/keymaps/jourloy/keymap.c
+++ keyboards/crkbd/keymaps/jourloy/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
      KC_LGUI,   KC_Q,  KC_W,     KC_F,          KC_P,   KC_B,      KC_J, KC_L, KC_U,     KC_Y,   KC_QUOTE, KC_RGUI,
      KC_LSHIFT, KC_A,  KC_R,     KC_S,          KC_T,   KC_G,      KC_M, KC_N, KC_E,     KC_I,   KC_O,     KC_RSHIFT,
-     KC_RALT,   KC_Z,  KC_X,     KC_C,          KC_D,   KC_V,      KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLSH,  KC_RALT,
+     KC_LCTRL,   KC_Z,  KC_X,     KC_C,          KC_D,   KC_V,      KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLSH,  KC_RALT,
                 MO(1), KC_SPACE, KC_ENT,        KC_TAB, KC_BSPACE, MO(1)
   ),
 
