6d6ef1916b99ef9d627bfae803666e9ec11063c5
Jourloy <jourloy@yandex.ru>
2022-08-17 01:05:42 +0300

    🔨 Fix RGB light

--- keyboards/crkbd/keymaps/jourloy/README.md
+++ keyboards/crkbd/keymaps/jourloy/README.md
@@ -2,22 +2,24 @@
 
 **Master hand:** left
 
-**Size:** 24618/28672 (85%, 4054 bytes free)
+**Size:** 24786/28672 (86%, 3886 bytes free)
 
 ## How to flash
 1. Compile `qmk compile -kb crkbd -km jourloy`
-2. Flash `qmk flash -kb crkbd -km jourloy ` (need to flash each keyboard)
+2. Flash `qmk flash -kb crkbd -km jourloy` (need to flash each keyboard)
 
 ## Layers
 - QWERTY - [miryoku](https://github.com/manna-harbour/miryoku/tree/master/docs/reference) layout
-- LOWER - numbers and symbols for programming
-- RAISE - system symbols
+- LOWER - numbers
+- RAISE - for programming
+- ADJUST - OS
 
 ## RGB
 RGB change color on each layer
-- QWERTY - RED
-- LOWER - CYAN
-- RAISE - PURPLE
+- QWERTY - AZURE
+- LOWER - PURPLE
+- RAISE - YELLOW
+- ASJUST - GREEN
 
 ## OLED
 Left oled contain information about current layer and WPM
--- keyboards/crkbd/keymaps/jourloy/keymap.c
+++ keyboards/crkbd/keymaps/jourloy/keymap.c
@@ -5,7 +5,8 @@
 enum layers {
   _QWERTY,
   _LOWER,
-  _RAISE
+  _RAISE,
+  _ADJUST
 };
 
 // Buttons
@@ -51,77 +52,112 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |   ⌘   |   Q   |   W   |   F   |   P   |   B   | |   J   |   L   |   U   |   Y   |   '   |   ⌘   |
+ * |  1/   |   Q   |   W   |   F   |   P   |   B   | |   J   |   L   |   U   |   Y   |   '   |   ♫↑  |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |  ⇧/   |   A   |   R   |   S   |   T   |   G   | |   M   |   N   |   E   |   I   |   O   |   ⇧/  |
+ * |   ⇥   |   A   |   R   |   S   |   T   |   G   | |   M   |   N   |   E   |   I   |   O   |   ♫x  |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |   ⌃   |   Z   |   X   |   C   |   D   |   V   | |   K   |   H   |   ,   |   .   |   /   |   ⌥   |
+ * |   ⌥   |   Z   |   X   |   C   |   D   |   V   | |   K   |   H   |   ,   |   .   |   /   |   ♫↓  |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- *                         |  1/   |   ␣   |   ↩   | |   ⇥   |   ⌫   |   1/  |
+ *                         |   ⌘   |   ␣   |   ⇧/  | |   ↩   |   ⌫   |   ⌃   |
  *                         +-------+-------+-------+ +-------+-------+-------+
  */
   [_QWERTY] = LAYOUT_split_3x6_3(
-     KC_LGUI,   KC_Q,  KC_W,     KC_F,          KC_P,   KC_B,      KC_J, KC_L, KC_U,     KC_Y,   KC_QUOTE, KC_RGUI,
-     KC_LSHIFT, KC_A,  KC_R,     KC_S,          KC_T,   KC_G,      KC_M, KC_N, KC_E,     KC_I,   KC_O,     KC_RSHIFT,
-     KC_LCTRL,   KC_Z,  KC_X,     KC_C,          KC_D,   KC_V,      KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLSH,  KC_RALT,
-                MO(1), KC_SPACE, KC_ENT,        KC_TAB, KC_BSPACE, MO(1)
+     MO(1),   KC_Q, KC_W, KC_F,    KC_P,     KC_B,           KC_J,   KC_L,      KC_U,     KC_Y,   KC_QUOTE, KC_KB_VOLUME_UP,
+     KC_TAB,  KC_A, KC_R, KC_S,    KC_T,     KC_G,           KC_M,   KC_N,      KC_E,     KC_I,   KC_O,     KC_KB_MUTE,
+     KC_LALT, KC_Z, KC_X, KC_C,    KC_D,     KC_V,           KC_K,   KC_H,      KC_COMMA, KC_DOT, KC_SLSH,  KC_KB_VOLUME_DOWN,
+                          KC_LGUI, KC_SPACE, KC_LSHIFT,      KC_ENT, KC_BSPACE, KC_LCTRL
   ),
 
 /* LOWER
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |       |   7   |   8   |   9   |   0   |       | |       |       |       |       |       |       |
+ * |       |   2/  |   3/  |       |       |       | |       |   1   |   2   |   3   |   \   |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |       |   4   |   5   |   7   |   =   |       | |       |   ;   |   `   |   \   |       |       |
+ * |       |       |       |       |       |       | |       |   4   |   5   |   6   |   ;   |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |       |   1   |   2   |   3   |   -   |       | |       |   [   |   ]   |   (   |   )   |       |
+ * |       |       |       |       |       |       | |       |   7   |   8   |   9   |   `   |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- *                         |       |  2/   |       | |       |   2/  |       |
+ *                         |       |   ⇧/  |       | |       |   0   |       |
  *                         +-------+-------+-------+ +-------+-------+-------+
  */
   [_LOWER] = LAYOUT_split_3x6_3(
-     XXXXXXX, KC_7, KC_8, KC_9,    KC_0,    XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,
-     XXXXXXX, KC_4, KC_5, KC_6,    KC_EQL,  XXXXXXX,      XXXXXXX, KC_SCLN, KC_GRV,  KC_BSLS,    XXXXXXX,    XXXXXXX,
-     XXXXXXX, KC_1, KC_2, KC_3,    KC_MINS, XXXXXXX,      XXXXXXX, KC_LBRC, KC_RBRC, LSFT(KC_9), LSFT(KC_0), XXXXXXX,
-                          KC_TRNS, MO(2),   XXXXXXX,      XXXXXXX,   MO(2), KC_TRNS
+     KC_TRNS, MO(2),   MO(3),   XXXXXXX,    XXXXXXX,       XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, KC_BSLS, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,       XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6, KC_SCLN, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,       XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, KC_GRV,  XXXXXXX,
+                       XXXXXXX, KC_LSHIFT,  XXXXXXX,       XXXXXXX, KC_0,    XXXXXXX
   ),
 
 /* RAISE
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |  ☼↑   |  ♫↑   |       |       |       |       | |       |       |   ⇧   |       |       |       |
+ * |       |       |   3/  |       |       |       | |       |       |       |       |       |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |       |  ♫x   |   ↞   |   ▶   |   ↠   |       | |       |   ⇦   |   ⇩   |   ⇨   |       |       |
+ * |       |       |       |       |       |       | |       |   [   |   ]   |   (   |   )   |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- * |  ☼↓   |  ♫↓   |       |       |       |       | |       |       |       |       |       |       |
+ * |       |       |       |       |       |       | |       |       |       |       |       |       |
  * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
- *                         |       |       |       | |       |       |       |
+ *                         |       |   ⇧/  |       | |       |       |       |
  *                         +-------+-------+-------+ +-------+-------+-------+
  */
   [_RAISE] = LAYOUT_split_3x6_3(
-     KC_BRMU, KC_KB_VOLUME_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-     XXXXXXX, KC_KB_MUTE,        KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-     KC_BRMD, KC_KB_VOLUME_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                 KC_TRNS, KC_TRNS, XXXXXXX,             XXXXXXX, KC_TRNS, KC_TRNS
+     KC_TRNS, KC_TRNS, MO(3),   XXXXXXX, XXXXXXX,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,       XXXXXXX, KC_LBRC, KC_RBRC, LSFT(KC_9), LSFT(KC_0), XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                XXXXXXX, KC_LSHIFT, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX
+  ),
+
+/* ADJUST
+ * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       | |       |       |   ⇧   |       |       |   ☼↑  |
+ * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       | |       |   ⇦   |   ⇩   |   ⇨   |       |       |
+ * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       | |       |       |       |       |       |   ☼↓  |
+ * +-------+-------+-------+-------+-------+-------+ +-------+-------+-------+-------+-------+-------+
+ *                         |       |       |       | |   ↞   |   ▶   |   ↠   |
+ *                         +-------+-------+-------+ +-------+-------+-------+
+ */
+  [_ADJUST] = LAYOUT_split_3x6_3(
+     KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_BRMU,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRMD,
+                                XXXXXXX, XXXXXXX, XXXXXXX,      KC_MPRV, KC_MPLY, KC_MNXT
   )
 };
 
 // RGB
 
 const rgblight_segment_t PROGMEM qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 54, HSV_RED}
+    {0, 6, HSV_RED},
+    {6, 21, HSV_AZURE},
+    {27, 6, HSV_RED},
+    {33, 21, HSV_AZURE}
 );
 
 const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 54, HSV_CYAN}
+    {1, 6, HSV_RED},
+    {6, 21, HSV_PURPLE},
+    {27, 6, HSV_RED},
+    {33, 21, HSV_PURPLE}
 );
 
 const rgblight_segment_t PROGMEM raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 54, HSV_PURPLE}
+    {1, 6, HSV_RED},
+    {6, 21, HSV_YELLOW},
+    {27, 6, HSV_RED},
+    {33, 21, HSV_YELLOW}
+);
+
+const rgblight_segment_t PROGMEM adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 6, HSV_RED},
+    {6, 21, HSV_GREEN},
+    {27, 6, HSV_RED},
+    {33, 21, HSV_GREEN}
 );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     qwerty_layer,
     lower_layer,
-    raise_layer
+    raise_layer,
+    adjust_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -136,47 +172,49 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
     rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
     return state;
 }
 
 // OLED
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
+  return OLED_ROTATION_270;
 }
 
 void oled_main(void) {
 
     // Layer
-    oled_write_P(PSTR("Layer: "), false);
+    oled_write_ln_P(PSTR("Layer"), false);
     switch (get_highest_layer(layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
+            oled_write_P(PSTR("qwr\n"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("Lower\n"), false);
+            oled_write_P(PSTR("lwr\n"), false);
             break;
         case _RAISE:
-            oled_write_P(PSTR("Raise\n"), false);
+            oled_write_P(PSTR("rse\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("adj\n"), false);
             break;
         default:
-            oled_write_ln_P(PSTR("Undefined"), false);
+            oled_write_ln_P(PSTR("Ukw"), false);
     }
+
+    oled_write_ln_P(PSTR("-----"), false);
 
     // WPM
     char buf[30];
     uint8_t wpm = get_current_wpm();
     sprintf(buf, "%u \n", wpm);
-    oled_write_P(PSTR("WPM: "), false);
+    oled_write_ln_P(PSTR("WPM"), false);
     oled_write(buf, false);
 }
 
 void oled_second(void) {
-    oled_write_ln_P(PSTR("\nAuthor: Jourloy"), false);
-    oled_write_ln_P(PSTR("Love: Nastya"), false);
+    oled_write_ln_P(PSTR("JRLY"), false);
 }
 
 bool oled_task_user(void) {
